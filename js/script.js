6b8e30acd9ae53d925def8341a50d1b05a4a8361
Tamara Siemieniuk <tamaras001@gmail.com>
2021-05-15 11:55:46 +0200

    js closed in functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-console.log("Hello World");
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let container = document.querySelector(".container");
-let themeName = document.querySelector(".js-themeName");
-changeBackgroundButton.addEventListener("click", () => {
-    container.classList.toggle("container--darkTheme");
-    themeName.innerText = container.classList.contains("container--darkTheme") ? "light" : "dark";
-});
-let showHideSubjectsButton = document.querySelector(".js-showHideSubjectsButton");
-let showHide = document.querySelector(".js-showHide");
-let removedunorderedList = document.querySelector(".unorderedList--removed");
-showHideSubjectsButton.addEventListener("click", () => {
-    removedunorderedList.classList.toggle("unorderedList");
-    showHide.innerText = removedunorderedList.classList.contains("unorderedList") ? "Hide" : "Show";
-});
+{
+    const welcome = () => { console.log("Hello World"); }
+
+
+    const toggleTheme = () => {
+        const container = document.querySelector(".container");
+        const themeName = document.querySelector(".js-themeName");
+
+        container.classList.toggle("container--darkTheme");
+        themeName.innerText = container.classList.contains("container--darkTheme") ? "light" : "dark";
+    };
+
+
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+        changeBackgroundButton.addEventListener("click", toggleTheme);
+    };
+    welcome();
+    init();
+}
+
+// coś jest źle w funkcji poniżej
+// ani nie działa rozwijanie tekstu
+// ani zamykanie w funkcje nie jest dobrze zrobione
+{
+    const toggleSubjects = () => {
+        const showHide = document.querySelector(".js-showHide");
+        const removedunorderedList = document.querySelector(".unorderedList--removed");
+
+        removedunorderedList.classList.toggle("unorderedList");
+        showHide.innerText = removedunorderedList.classList.contains("unorderedList") ? "Hide" : "Show";
+    };
+
+    {const subjects = () => {
+        const showHideSubjectsButton = document.querySelector(".js-showHideSubjectsButton");
+    showHideSubjectsButton.addEventListener("click", toggleSubjects);
+    };
+    subjects();
+
+}
